0599a81ce12d0c5829d1caa457e4b42b1f6bb18f
André Velásquez <avela504x@gmail.com>
2024-12-05 21:52:38 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 let config = {
-    apiKey: "AIzaSyDF3_-QOhYNjiOZguvMSKAzNTJh2e1WPq0",
+    apiKey: "",
     authDomain: "niveltanque-17dc5.firebaseapp.com",
     databaseURL: "https://niveltanque-17dc5.firebaseio.com",
     projectId: "niveltanque-17dc5",
     storageBucket: "niveltanque-17dc5.appspot.com",
-    messagingSenderId: "381208732354",
-    appId: "1:381208732354:web:7142efc0303c93bfee115b"
+    messagingSenderId: "",
+    appId: ""
 };
 
 let app = firebase.initializeApp(config);
